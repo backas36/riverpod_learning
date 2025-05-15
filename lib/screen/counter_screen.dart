e16f9b5e9d6a1c9b101cb09f54274906b2907757
backas36 <backas36@gmail.com>
2025-05-16 01:08:22 +0800

    improve UI

--- lib/screen/counter_screen.dart
+++ lib/screen/counter_screen.dart
@@ -106,119 +106,134 @@ class _CounterScreenState extends ConsumerState<CounterScreen> {
                         ],
                       );
                     },
-                    error: (error, stack) => Text(error.toString()),
-                    loading: () => const CircularProgressIndicator(),
+                    error:
+                        (error, stack) => Container(
+                          height: 120,
+                          alignment: Alignment.center,
+                          child: Text(error.toString()),
+                        ),
+                    loading:
+                        () => Container(
+                          height: 120,
+                          alignment: Alignment.center,
+                          child: const CircularProgressIndicator(),
+                        ),
                   ),
                 ),
               ),
               const SizedBox(height: 48),
               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Column(
-                    children: [
-                      FloatingActionButton(
-                        heroTag: 'decrement',
-                        onPressed: () {
-                          //ref.read(counterStateProvider.notifier).state--;
-                          ref
-                              .read(counterAsyncNotifierProvider.notifier)
-                              .decrement();
-                        },
-                        backgroundColor: Colors.redAccent,
-                        elevation: 4,
-                        child: const Icon(Icons.remove, size: 32),
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        'Decrease',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500, // 較細
-                          color: Colors.redAccent,
-                          letterSpacing: 1.2,
+                  Expanded(
+                    child: Column(
+                      children: [
+                        FloatingActionButton(
+                          heroTag: 'decrement',
+                          onPressed: () {
+                            //ref.read(counterStateProvider.notifier).state--;
+                            ref
+                                .read(counterAsyncNotifierProvider.notifier)
+                                .decrement();
+                          },
+                          backgroundColor: Colors.redAccent,
+                          elevation: 4,
+                          child: const Icon(Icons.remove, size: 32),
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 10),
+                        Text(
+                          'Decrease',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500, // 較細
+                            color: Colors.redAccent,
+                            letterSpacing: 1.2,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  const SizedBox(width: 26),
-                  Column(
-                    children: [
-                      FloatingActionButton(
-                        heroTag: 'increment',
-                        onPressed: () {
-                          //ref.read(counterStateProvider.notifier).state++;
-                          ref
-                              .read(counterAsyncNotifierProvider.notifier)
-                              .increment();
-                        },
-                        backgroundColor: Colors.green,
-                        elevation: 4,
-                        child: const Icon(Icons.add, size: 32),
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        'Increase',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500, // 較細
-                          color: Colors.green,
-                          letterSpacing: 1.2,
+                  Expanded(
+                    child: Column(
+                      children: [
+                        FloatingActionButton(
+                          heroTag: 'increment',
+                          onPressed: () {
+                            //ref.read(counterStateProvider.notifier).state++;
+                            ref
+                                .read(counterAsyncNotifierProvider.notifier)
+                                .increment();
+                          },
+                          backgroundColor: Colors.green,
+                          elevation: 4,
+                          child: const Icon(Icons.add, size: 32),
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 10),
+                        Text(
+                          'Increase',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500, // 較細
+                            color: Colors.green,
+                            letterSpacing: 1.2,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  const SizedBox(width: 26),
-                  Column(
-                    children: [
-                      FloatingActionButton(
-                        heroTag: 'reset',
-                        onPressed: () {
-                          ref
-                              .read(counterAsyncNotifierProvider.notifier)
-                              .reset();
-                        },
-                        backgroundColor: Colors.amber,
-                        elevation: 4,
-                        child: const Icon(Icons.refresh, size: 32),
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        'Reset',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500, // 較細
-                          color: Colors.amber,
-                          letterSpacing: 1.2,
+                  Expanded(
+                    child: Column(
+                      children: [
+                        FloatingActionButton(
+                          heroTag: 'reset',
+                          onPressed: () {
+                            ref
+                                .read(counterAsyncNotifierProvider.notifier)
+                                .reset();
+                          },
+                          backgroundColor: Colors.amber,
+                          elevation: 4,
+                          child: const Icon(Icons.refresh, size: 32),
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 10),
+                        Text(
+                          'Reset',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500, // 較細
+                            color: Colors.amber,
+                            letterSpacing: 1.2,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  const SizedBox(width: 26),
-                  Column(
-                    children: [
-                      FloatingActionButton(
-                        heroTag: 'error',
-                        onPressed: () {
-                          ref
-                              .read(counterAsyncNotifierProvider.notifier)
-                              .beError();
-                        },
-                        backgroundColor: Colors.blueAccent,
-                        elevation: 4,
-                        child: const Icon(Icons.error, size: 32),
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        'Error',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500, // 較細
-                          color: Colors.blueAccent,
-                          letterSpacing: 1.2,
+                  Expanded(
+                    child: Column(
+                      children: [
+                        FloatingActionButton(
+                          heroTag: 'error',
+                          onPressed: () {
+                            ref
+                                .read(counterAsyncNotifierProvider.notifier)
+                                .beError();
+                          },
+                          backgroundColor: Colors.blueAccent,
+                          elevation: 4,
+                          child: const Icon(Icons.error, size: 32),
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 10),
+                        Text(
+                          'Error',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500, // 較細
+                            color: Colors.blueAccent,
+                            letterSpacing: 1.2,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
